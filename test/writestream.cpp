6235b5f6c1388e33e837d640240edd9c98b7f741
Giulio Eulisse <sha1-a598d8593b5db48d5829590a227bbe04f25460b4@cern.ch>
2005-11-22 17:21:46 +0000

    * .admin/developers: administrators added on pete's request.
    * BuildFile, DCacheStorageMaker.h, GsiFTPStorageMaker.h,
    HttpStorageMaker.h, LocalStorageMaker.h, RFIOStorageMaker.h,
    RedirectStorageMaker.h, StorageAccount.h, StorageAccountProxy.h,
    StorageFactory.h, StorageMaker.h, ZipMemberStorageMaker.h,
    DCacheStorageMaker.cc, GsiFTPStorageMaker.cc, HttpStorageMaker.cc,
    LocalStorageMaker.cc, RFIOStorageMaker.cc,
    RedirectStorageMaker.cc, StorageAccount.cc,
    StorageAccountProxy.cc, StorageFactory.cc, StorageMaker.cc,
    ZipMemberStorage.cc, module.cc, BuildFile, any.cpp, ftp.cpp,
    ftp2.cpp, http.cpp, http2.cpp, http3.cpp, http4.cpp, local.cpp,
    local2.cpp, local3.cpp, redirect.cpp, rfio.cpp, rfio2.cpp,
    rfio3.cpp, rfzip.cpp, rfzip2.cpp, rfzip3.cpp, write.cpp,
    writestream.cpp, zip.cpp, zip2.cpp, zip3.cpp, ziplist.cpp: files
    imported in the repository.

---
+++ test/writestream.cpp
@@ -0,0 +1,54 @@
+//<<<<<< INCLUDES                                                       >>>>>>
+
+#include "Utilities/Configuration/interface/Architecture.h"
+#include "Utilities/StorageFactory/interface/StorageFactory.h"
+#include "Utilities/StorageFactory/interface/StorageAccount.h"
+#include "PluginManager/PluginManager.h"
+#include "SealIOTools/StorageStreamBuf.h"
+#include "SealBase/DebugAids.h"
+#include "SealBase/Signal.h"
+#include "SealBase/Error.h"
+#include "SealBase/File.h"
+#include <iostream>
+
+//<<<<<< PRIVATE DEFINES                                                >>>>>>
+//<<<<<< PRIVATE CONSTANTS                                              >>>>>>
+//<<<<<< PRIVATE TYPES                                                  >>>>>>
+//<<<<<< PRIVATE VARIABLE DEFINITIONS                                   >>>>>>
+//<<<<<< PUBLIC VARIABLE DEFINITIONS                                    >>>>>>
+//<<<<<< CLASS STRUCTURE INITIALIZATION                                 >>>>>>
+//<<<<<< PRIVATE FUNCTION DEFINITIONS                                   >>>>>>
+//<<<<<< PUBLIC FUNCTION DEFINITIONS                                    >>>>>>
+//<<<<<< MEMBER FUNCTION DEFINITIONS                                    >>>>>>
+
+using namespace seal;
+int main (int, char **argv)
+{
+    Signal::handleFatal (argv [0]);
+    PluginManager::get ()->initialise ();
+
+    try
+    {
+	std::string		user (UserInfo::self ()->id ());
+	std::string		path (std::string ("rfio:/castor/cern.ch/user/")
+			       	      + user[0] + "/" + user + "/rfiotest");
+    	Storage			*s = StorageFactory::get ()->open
+	    (path.c_str (), IOFlags::OpenWrite | IOFlags::OpenCreate | IOFlags::OpenTruncate);
+	StorageStreamBuf	buf (s);
+	std::ostream		output (&buf);
+
+	for (int i = 0; i < 100; ++i)
+	    output << "sample output line " << i << std::endl;
+
+	output.flush ();
+	s->close ();
+    }
+    catch (Error &e)
+    {
+	std::cerr << e.explain () << std::endl;
+	return EXIT_FAILURE;
+    }
+
+    std::cerr << "stats:\n" << StorageAccount::summaryText ();
+    return EXIT_SUCCESS;
+}
